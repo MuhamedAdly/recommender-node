2fad7a5459859a150cfc0a5999294b5f7b0f3170
Mohamed Adly <muhamedadly@gmail.com>
2023-09-03 12:20:17 +0300

    Update index.js
    removed setup

--- index.js
+++ index.js
@@ -8,35 +8,6 @@ var fs = require('fs');
 var datamodel = require('./lib/datamodel');
 var recommender = require('./lib/recommender');
 
-
-nodeRecommender.setup = function (path, k, clusterPath) {
-    return new Promise(function (resolve, reject) {
-        datamodel.loadDataset(path).then(
-            (data) => {
-                var users = data.users;
-                var items = data.items;
-                datamodel.getClusters(data.matrix, k).then(
-                    (clusters) => {
-                        var fileData = {
-                            users: users.colArray(),
-                            items: items.values,
-                            clusters: clusters
-                        }
-                        fs.writeFile(clusterPath, JSON.stringify(fileData), function (err) {
-                            if (!err) {
-                                resolve(users.colArray());
-                            } else {
-                                reject(err);
-                            }
-                        });
-                    }
-                ).catch((err) => reject(err));
-            }
-        ).catch((err) => reject(err));
-    });
-
-}
-
 nodeRecommender.setupFromArray = function (dataAray, k, clusterPath) {
     return new Promise(function (resolve, reject) {
         datamodel.loadDatasetFromArray(dataAray).then(
